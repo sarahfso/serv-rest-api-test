4672d0386693c9589e52f196323ead38255c3b81
Sarah Ferreira da Silva <sarahfssilva@hotmail.com>
2023-11-14 18:05:29 -0300

    testing multi specs

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   reporter: "cypress-multi-reporters",
   reporterOptions: {
-    configFile: "reporter-config.json"
+    configFile: "reporter-config.json",
+    mochaFile: "./cypress/results/cypress-output.[hash].xml"
   },
   e2e: {
     baseUrl: 'https://serverest.dev',
